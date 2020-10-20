0deac041121f922e087bca82d6392e9c5e3dc972
10196470 <wang.ping6@zte.com.cn>
2020-10-20 14:42:05 +0800

    init i-one

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="one-app"></div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped lang="less">
+.one-app {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ test/unit/jest.conf.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  rootDir: path.resolve(__dirname, '../../'),
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/test/e2e'
+  ],
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  setupFiles: ['<rootDir>/test/unit/setup'],
+  mapCoverage: true,
+  coverageDirectory: '<rootDir>/test/unit/coverage',
+  testRegex: '(<rootDir>/test/unit/specs/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  collectCoverageFrom: [
+    'src/components/vc-cat/*.{js,vue}',
+    '!src/components/vc-cat/index.js',
+    //'!src/**/*.{js,vue}',
+    '!src/main.js',
+    '!src/router/index.js',
+    '!**/node_modules/**'
+  ],
+  'testURL': 'http://localhost'
+}
